439837c777b92b13c6368d37ef62cb5afd37279f
Sunny Singh <ssingh.math@gmail.com>
2014-12-04 15:28:29 -0500

    Pixel RGB colormap produced by a SHA-224 hash of unique tokens

--- py/dotplot.py
+++ py/dotplot.py
@@ -1,9 +1,19 @@
 import Image
 import string
 import sys
+import hashlib
 from glob import glob
 
 exclude = set(string.punctuation)
+
+def color_hash(sequence):
+	tokens = list(set(sequence))
+	colormap = {}
+	for token in tokens:
+		hashed = hashlib.sha224(token).hexdigest()
+		r,g,b = hashed[:2], hashed[2:4], hashed[4:6]
+		colormap[token] = (int(r,16), int(g, 16), int(b, 16))
+	return colormap
 
 # Simple tokenizer. Removes punctuation, lowercases and splits on whitespace
 def read_file(fname):
@@ -14,18 +24,19 @@ def read_file(fname):
 # Produces an NxN dotplot of a sequence against itself
 def generate_dp(sequence, outpath):
 	N = len(sequence)
+	colormap = color_hash(sequence)
 	img = Image.new("RGB", (N, N), "white")
 	pixels = img.load()
 
 	for i in range(N):
 		for j in range(N):
 			if sequence[i]==sequence[j]:
-				pixels[i, N-j-1] = (0, 0, 0)
+				pixels[i, N-j-1] = colormap[sequence[i]]
 	img.save(outpath+".png")
 
 # Batch run over all text files in an input directory
 def run():
-	files = glob("%s\\*" % sys.argv[1])
+	files = glob("%s\\*.txt" % sys.argv[1])	
 	for f in files:
 		sequence = read_file(f)
 		outpath = f.split('.')[0]
